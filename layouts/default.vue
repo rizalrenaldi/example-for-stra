eb858e52a0152e3e8263f11d140b540a26aa46f4
rizalrenaldi <rizalrenaldi@gmail.com>
2022-03-18 03:01:58 +0700

    init

---
+++ layouts/default.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="antialiased">
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <main class="h-screen flex items-center justify-center">
+    <div class="container mx-auto p-4">
+      <h1
+        class="
+          text-3xl
+          sm:text-4xl
+          md:text-5xl
+          lg:text-6xl
+          xl:text-7x
+          2xl:text-8xl
+          font-bold
+          tracking-tight
+        "
+      >
+        Nuxt/Tailwind
+      </h1>
+      <p
+        class="
+          text-3xl
+          sm:text-4xl
+          md:text-5xl
+          lg:text-6xl
+          xl:text-7x
+          2xl:text-8xl
+          font-semibold
+          tracking-tight
+          text-black/40
+        "
+      >
+        Barebone Nuxt 2 with Tailwind 3 boilerplate with no linting, no testing,
+        no fuss, nothing, because why not.
+      </p>
+    </div>
+  </main>
+</template>
+
+<script>
+export default {};
+</script>
